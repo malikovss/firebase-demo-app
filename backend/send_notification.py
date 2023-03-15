71cc91dc00abb5bd2378744b4158167775762ed2
Suhrob <mlkv.dev@gmail.com>
2023-03-15 18:35:34 +0500

    Initial commit

--- backend/send_notification.py
+++ backend/send_notification.py
@@ -3,12 +3,14 @@ import os
 import firebase_admin
 from firebase_admin import messaging
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "serviceAccountKey.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 default_app = firebase_admin.initialize_app()
-message = messaging.Message(notification=messaging.Notification(
-    title='title',
-    body='body',
-    image='https://i.dummyjson.com/data/products/10/1.jpg',
-),
-    token='device-token')
+message = messaging.Message(
+    data={'test': 'test', 'language': 'uz'},
+    notification=messaging.Notification(
+        title='title',
+        body='body',
+        image='https://i.dummyjson.com/data/products/10/1.jpg',
+    ),
+    token='eQwo94QrT6mAcxZ-EiE5vq:APA91bHhZrFLXgzHORjpg02eF-P9e-Bxalw7YGvni-3xwPFFrZ6U948rp-5iwxdbzURBJHnpoTBCCKW2q8FSie_fMocOb7vwVpEFyLEY5lfMtJADWXciDzQHI2r2myGKgLyiMkAEdrB0')
 messaging.send(message)
